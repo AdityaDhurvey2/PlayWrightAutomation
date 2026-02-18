1e3b87fd493423302f3e5bc0e04e72378fe1a6d0
AdityaDhurvey2 <aditya.dhurvey@redhutechnology.in>
2026-02-18 11:29:22 +0530

    Error fix in ClientAppPO file

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
-const { LoginPage } = require("../pageObjects/LoginPage");
-const { DashboardPage } = require("../pageObjects/DashboardPage");
+const { LoginPage } = require("../pageobjects/LoginPage");
+const { DashboardPage } = require("../pageobjects/DashBoardPage");
 
 test("Client App login", async ({ page }) => {
   //js file- Login js, DashboardPage
